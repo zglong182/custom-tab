58e1b7f752697e6c11420527687b12820c7995ed
zglong <zglong182@163.com>
2021-04-09 00:43:49 +0800

    feat: sql

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
--- ----------------------------
--- Records of tb_user
--- ----------------------------
-INSERT INTO `tb_user` VALUES ('1', 'laowang', '112233');
-INSERT INTO `tb_user` VALUES ('2', 'laoli', '123456');
